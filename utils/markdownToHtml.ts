3b8cfcfbf789a54d656ef6cb300377d987d6496c
Akamig <akamig@posteo.net>
2020-09-09 03:00:06 +0900

    changed mdtohtml to enable html in md

--- utils/markdownToHtml.ts
+++ utils/markdownToHtml.ts
@@ -1,12 +1,14 @@
 import unified from 'unified';
 import mdparse from 'remark-parse';
 import remark2rehype from 'remark-rehype';
+import raw from 'rehype-raw'
 import html from 'rehype-stringify';
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(mdparse)
-    .use(remark2rehype)
+    .use(remark2rehype, { allowDangerousHtml: true })
+    .use(raw)
     .use(html)
     .process(markdown);
   return result.toString();
