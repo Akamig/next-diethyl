5075168ac25749f3cd52456f1f5b1bb225d0ae50
Akamig <akamig@posteo.net>
2020-09-09 02:23:23 +0900

    add Pagenation supporting query API

--- utils/api.ts
+++ utils/api.ts
@@ -45,7 +45,7 @@ async function fetchAPI(query, { variables } = {}) {
 export async function getAllPosts() {
   const data = await fetchAPI(`query Posts {
     posts{
-    ${postQuery}
+      ${postQuery}
     }
   }`);
   return data.posts;
@@ -114,8 +114,8 @@ export async function getPostsByTag(slug) {
           name
           color
           posts{
-          ${postQuery}
-        }
+            ${postQuery}
+          }
         }
       }`,
     { variables: { slug } }
@@ -134,4 +134,42 @@ export async function getPostsByCategory(slug) {
     { variables: { slug } }
   );
   return data.categories[0];
+}
+
+export async function PagedAllPosts(page: number) {
+  const perPage = +process.env.PER_PAGE;
+  const curPage = perPage * page - perPage || 0;
+  const data = await fetchAPI(
+    `query posts ($curPage:Int!, $perPage:Int!){
+      posts(limit: $perPage, start: $curPage){
+        ${postQuery}
+      }
+      postsCount
+    }
+  `,
+    { variables: { curPage, perPage } }
+  );
+  data.lastPage = Math.ceil(data.postsCount / perPage);
+
+  return data;
+}
+
+export async function PagedPostBySlug(
+  page: number,
+  slug: string,
+  queryTarget: string
+) {
+  const perPage = +process.env.PER_PAGE;
+  const curPage = perPage * (page - 1) || 1;
+  const data: posts = await fetchAPI(
+    `query posts ($curPage:Int!, $perPage:Int!, $slug:String!){
+      posts(where: {${queryTarget}: {slug: $slug}} limit: $perPage, start: $curPage){
+        ${postQuery}
+      }
+    }
+  `,
+    { variables: { curPage, perPage, slug } }
+  );
+  data.lastPage = Math.ceil(data.posts.length / perPage);
+  return data;
 }
