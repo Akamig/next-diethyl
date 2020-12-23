9a21d76c960958ca32c7133601b746a8f2a266a1
Akamig <akamig@posteo.net>
2020-12-23 22:15:30 +0900

    change to styled component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,20 +1,39 @@
 import React, { ReactNode } from 'react';
-import Link from 'next/link';
+import styled from '@emotion/styled';
 import Meta from '../Meta/meta';
 import Sidebar from '../navbar/sidebar';
 import Footer from '../Footer/footer';
+
+const Wrapper = styled.div`
+  display: grid;
+  place-content: center;
+  @media screen and (min-width: 45rem) {
+    grid-template: min-content 2rem / min-content minmax(45rem, 56.25rem);
+    margin: 2rem;
+  }
+  @media screen and (min-width: 25rem) {
+    grid: ;
+  }
+`;
+
+const Contents = styled.main``;
+
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title }: Props) => (
-  <>
-    <Meta title={title}/>
-    <Sidebar />
-    <main>{children}</main>
-    <Footer />
-  </>
-);
+const Layout = ({ children, title }: Props) => {
+  return (
+    <>
+      <Meta title={title} />
+      <Wrapper>
+        <Sidebar />
+        <Contents>{children}</Contents>
+        <Footer />
+      </Wrapper>
+    </>
+  );
+};
 
 export default Layout;
--- components/navbar/sidebar.tsx
+++ components/navbar/sidebar.tsx
@@ -1,43 +1,69 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Link from 'next/link';
+
+const SidebarWrapper = styled.div`
+  display: grid;
+  @media (min-width: 45rem) {
+    grid-template: 8rem max-content / 8rem auto;
+    border: 1px solid;
+    height: min-content;
+  }
+  @media (min-width: 45rem) {
+    
+  }
+`;
+
+const Logo = styled.div`
+  place-self: center;
+`;
+
+const Navigation = styled.nav`
+`;
+
+const Seperator = styled.div`
+  height: 10rem;
+  width: 0.5rem;
+  background-color: #fff;
+  margin: 0 0.5rem;
+`;
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <h1 className='logo'>
+    <SidebarWrapper>
+      <Logo>
         <Link href='/'>
           <a>
             <img src='/favicon.svg' alt='Test!' style={{ width: '100px' }} />
           </a>
         </Link>
-      </h1>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link href='/post'>
-                <a>Post</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/category'>
-                <a>Category</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/badge'>
-                <a>Badge</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/about'>
-                <a>About</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+      </Logo>
+      <Seperator />
+      <Navigation>
+        <ul>
+          <li>
+            <Link href='/post'>
+              <a>Post</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/category'>
+              <a>Category</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/badge'>
+              <a>Badge</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </Navigation>
+    </SidebarWrapper>
   );
 };
 
