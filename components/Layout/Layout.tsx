5f75d5621ef1f65a4b3182c34f3b35ad006b7d76
Akamig <akamig@posteo.net>
2021-01-13 21:53:44 +0900

    Styled component, adjust grid

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from 'react';
+import styled from '@emotion/styled';
+
 import Link from 'next/link';
 import Meta from '../Meta/meta';
 import Sidebar from '../navbar/sidebar';
@@ -8,13 +10,38 @@ type Props = {
   title?: string;
 };
 
+const Main = styled.div`
+  display: grid;
+
+  @media screen and (max-width: 900px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto auto auto;
+  }
+  @media screen and (min-width: 900px) {
+    grid-template-columns: 100px 1fr;
+    grid-template-rows: auto auto auto;
+    position: 50vw;
+    max-width: 1000px;
+  }
+  margin: 1rem 1rem;
+  padding: 1rem;
+
+  & > * {
+    border: 1px solid black;
+  }
+
+  main {
+    padding: 0.625rem;
+  }
+`;
+
 const Layout = ({ children, title }: Props) => (
-  <>
-    <Meta title={title}/>
+  <Main>
+    <Meta title={title} />
     <Sidebar />
     <main>{children}</main>
     <Footer />
-  </>
+  </Main>
 );
 
 export default Layout;
