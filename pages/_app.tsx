625a8d1363a6d684fc0a765bcce3e9b6ad5113f3
Akamig <akamig@posteo.net>
2021-03-03 22:40:58 +0900

    Add webfont css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,21 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             name='viewport'
             content='initial-scale=1.0, width=device-width'
           />
+          <link
+            href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css'
+            rel='stylesheet'
+            type='text/css'
+          />
+          <link
+            href='//rsms.me/inter/inter.css'
+            rel='stylesheet'
+            type='text/css'
+          ></link>
+                    <link
+            href='//rsms.me/inter/inter-display.css'
+            rel='stylesheet'
+            type='text/css'
+          ></link>
         </Head>
         <Component {...pageProps} />
       </Theme>
