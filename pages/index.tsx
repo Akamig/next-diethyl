0dcbc9e4601ea786c408cc6356e85fc767338e53
Akamig <akamig@posteo.net>
2021-03-03 22:41:41 +0900

    Remove Unused container & useTheme hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { useTheme } from '@emotion/react';
 
-import Link from 'next/link';
 import Layout from '../components/Layout/Layout';
-import Container from 'components/Container/container';
 import PostListContainer from 'components/PostListContainer/post-list-container';
 import { getAllPosts } from 'utils/api';
 
 export default function IndexPage({ posts }) {
-  const theme = useTheme();
   return (
     <Layout title='Ethyl'>
       <PostListContainer posts={posts} />
