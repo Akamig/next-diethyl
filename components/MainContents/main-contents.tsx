2f2f16a4ab7bdc220fc5703e1396cd1ab45e97d3
Akamig <akamig@posteo.net>
2021-03-08 05:17:58 +0900

    Add main-contents component

---
+++ components/MainContents/main-contents.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Main = styled.div`
+  background-color: ${(props) => props.theme.color.sub};
+  min-height: 100vh;
+  flex: 1 0 0;
+  padding: 1rem;
+`;
+
+const MainContents = ({ children }: Props) => {
+  return (
+    <Main>
+      {children}
+    </Main>
+  );
+};
+
+export default MainContents;
