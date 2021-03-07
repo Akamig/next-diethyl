8f3d037f9f8bcf840dc84a84a5bda82db0e9ae45
Akamig <akamig@posteo.net>
2021-03-08 07:12:41 +0900

    Flexboxed Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,31 +13,46 @@ type Props = {
 };
 
 const Main = styled.div`
-  display: grid;
-  grid-template-rows: auto auto auto;
-  @media screen and (max-width: 900px) {
-    grid-template-columns: auto;
-    overflow-x: hidden;
+  margin: 0 auto;
+  overflow-x: hidden;
+  ${(props) => props.theme.mediaQuery.sm} {
+    padding: 0.5rem 0;
   }
+`;
 
-  @media screen and (min-width: 900px) {
-    grid-template-columns: 100px auto;
-    max-width: 1000px;
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1 0 auto;
+  margin: 0 auto;
+  max-width: 1100px;
+
+  ${(props) => props.theme.mediaQuery.sm} {
+    & > * {
+      margin: 0.5rem;
+      margin-top: 0;
+    }
+  }
+  ${(props) => props.theme.mediaQuery.md} {
+    aside {
+      margin-right: 0;
   }
 `;
 
 const Layout = ({ children, title }: Props) => {
   return (
-  <Main>
+    <Main>
       <Head>
         <title>{title}</title>
       </Head>
+      <Container>
         <Header />
         <Navbar />
         <MainContents>{children}</MainContents>
         <Footer />
-  </Main>
-);
+      </Container>
+    </Main>
+  );
 };
 
 export default Layout;
