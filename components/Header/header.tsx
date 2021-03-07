419447533c33883cf04b48b41ae601de1ab73592
Akamig <akamig@posteo.net>
2021-03-08 05:25:41 +0900

    Add header component

---
+++ components/Header/header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Box = styled.div`
+  background-color: #121212;
+  flex-basis: 100%;
+  margin: 0.5rem;
+  padding: 0.25rem 0.5rem;
+`;
+const Text = styled.p`
+  font-family: 'InterDisplay';
+  display: inline-block;
+  font-weight: 600;
+  font-size: 1.25rem;
+  line-height: 1;
+  letter-spacing: -0.02rem;
+  padding: 0.125rem;
+  margin-right: 0.25rem;
+  vertical-align: middle;
+  color: #eec;
+`;
+
+const Header = ({}) => {
+  return (
+    <Box>
+      <Text>Akamig</Text>
+    </Box>
+  );
+};
+
+export default Header;
